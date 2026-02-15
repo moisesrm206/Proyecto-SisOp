a91531ef262f647ef13b084617f7acca256fc582
Moises Ramon Mendez C. <24030374@itcelaya.edu.mx>
2026-02-15 12:54:44 -0600

    bug-fix and feat: Agregar scroll a la tabla, ademas se añadio un grafo de las ramas del repositorio (se puede visualizar con la extension de mermaid), y se añadio una funcion para poder actualizar la tabla cada que se crea o borra un proceso.

--- gui/interfaz.py
+++ gui/interfaz.py
@@ -1,4 +1,3 @@
-
 import customtkinter as ctk
 from tkinter.ttk import Treeview as TreeVw
 import time as tm
@@ -12,7 +11,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.Proceso import proceso as prcs
 
 #Lista de procesos (sin tamaño máximo definido)
-procesos = []
+procesos: list[prcs] = []
 
 def CrearProceso():
     PrcssNmDialog = ctk.CTkInputDialog(title="Crear Proceso", text="Ingrese el nombre del proceso:")
@@ -21,11 +20,16 @@ def CrearProceso():
     PrcssTamDialog = ctk.CTkInputDialog(title="Crear Proceso", text="Ingrese el tamaño del proceso:")
     tamProceso = PrcssTamDialog.get_input()
     
+    # TODO: Revisar o cambiar el tipo de dato, ya que el formato de fecha no es algo muy deseado al mostrarse en tabla, 
+    # TODO: un ejemplo de como sale es: 2026 02 15 12 35 20 05 06, esto es año, mes, dia, hora, minuto, segundo, y probablemente milisegundos,
+    # TODO: revisar si es posible mostrar solo la hora, o el formato de fecha deseado, o si es necesario cambiar el tipo de dato a string y mostrar la fecha en el formato deseado
+        
     tiempo_llegada = tm.localtime()
     print(tiempo_llegada)
 
     proceso = prcs(nombrePrcs, True, tamProceso, 0, 0, tiempo_llegada, 0, 0, 0)
     procesos.append(proceso)
+    actualizar_tabla()
 
 def BorrarProceso():
     BorrarDialog = ctk.CTkInputDialog(title="Borrar Proceso", text="Ingrese el nombre del proceso a borrar:")
@@ -36,7 +40,7 @@ def BorrarProceso():
             break
         else:
             print("Proceso no encontrado")
-            
+    actualizar_tabla()
 
 app = ctk.CTk()
 
@@ -70,12 +74,29 @@ tabla.heading(5, text="Finalización")
 tabla.heading(6, text="Tiempo de atención")
 tabla.heading(7, text="Tiempo de espera")
 
-''' #TODO: Agregar un loop para mostrar los procesos en la tabla, y actualizar la tabla cada vez que se cree o borre un proceso
-    tambien cambiar la forma en la que se muestra la tabla para que no se vaya mas alla del borde del frame de estado de memoria,
-    y agregar un scroll para la tabla en caso de que se agreguen muchos procesos.
-'''
+tabla.column(1, width=60)
+tabla.column(2, width=50)
+tabla.column(3, width=50)
+tabla.column(4, width=60)
+tabla.column(5, width=80)
+tabla.column(6, width=110)
+tabla.column(7, width=120)
+
+# ? INFO: El scrollbar horizontal no funciona, no se muestran barras para mover, pero si funciona el vertical,
+# ? no se muestra la barra tampoco, pero se puede mover usando la rueda del raton
+sb_y= ctk.CTkScrollbar(app, orientation="vertical", command=tabla.yview)
+tabla.configure(yscrollcommand=sb_y.set)
+sb_x = ctk.CTkScrollbar(app, orientation="horizontal", command=tabla.xview)
+tabla.configure(xscrollcommand=sb_x.set)
+
+def actualizar_tabla():
+    for i in tabla.get_children():
+        tabla.delete(i)
+    for proceso in procesos:
+        tabla.insert("", "end", values=(proceso.nombre, proceso.estado, proceso.tamano, proceso.tiempo_llegada, proceso.tiempo_finalizacion, proceso.tiempo_atencion, proceso.tiempo_espera))
+
 
 tabla.grid(padx = 150, pady = 80, column=1)
 
-app.mainloop()
 
+app.mainloop()
